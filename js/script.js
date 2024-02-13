acd28e0b47b6dec028fecfa6ed5854aced0b4b42
roberto-palanga <r.palanga94@gmail.com>
2024-02-13 15:42:30 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team,
+ le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+Organizzare i singoli membri in card/schede
+
+*/
